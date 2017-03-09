7f9addbe751a7780b76db58e4b4701436ec5b39f
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-03-09 18:10:06 +0000

    refactored

--- server.js
+++ server.js
@@ -4,16 +4,14 @@ const express = require('express');
 const WebSocket = require('ws');
 const SocketServer = require('ws').Server;
 const uuid = require('node-uuid');
-// Set the port to 3001
 const PORT = 3001;
 
-// Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
+
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// Create the WebSockets server
+
 const wss = new SocketServer({ server });
 
 function extractUrl(message) {
@@ -25,53 +23,41 @@ function extractUrl(message) {
   return;
 }
 
+function broadCast(message) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  });
+}
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
+
 wss.on('connection', (ws) => {
 
   console.log('Client connected');
-  console.log(wss.clients.size);
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({connections: wss.clients.size}));
-    }
-  });
-
+  broadCast({connections: wss.clients.size});
   ws.on('message', function incoming(message) {
-
     const data = JSON.parse(message);
     const output = {
-      color: data.color,
       content: data.content,
-      username: data.username
+      id: uuid.v1()
     };
-    extractUrl(output);
+
     if (data.type === "postMessage") {
       output.type = "incomingMessage";
+      output.color = data.color;
+      output.username = data.username;
+      extractUrl(output);
     }
     if (data.type === "postNotification") {
       output.type = "incomingNotification";
     }
-
-    output.id = uuid.v1();
-    console.log(output);
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(output));
-      }
-    });
+    broadCast({message: output});
   });
 
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+
   ws.on('close', () => {
     console.log('Client disconnected');
-
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({connections: wss.clients.size}));
-      }
-    });
+    broadCast({connections: wss.clients.size});
   });
 });
